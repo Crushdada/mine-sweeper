a5bf6f25f431df99a1889c1440160418c911cb3b
David <2265997266@qq.com>
2022-05-21 21:58:10 +0800

    feat(index.vue): init Minefield state

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
-
+interface BolckState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean
+  flagged?: boolean
+  adjacentMines?: number
+}
+const WIDTH = 10;
+const HEIGHT = 10;
+const state = reactive(
+  Array.from({ length: HEIGHT }, (_, y) =>
+    Array.from({ length: WIDTH }, (_, x): BolckState => ({ x, y }))),
+)
+function onClick(x: number, y: number) {
+  console.log(x, y)
+}
 </script>
 
 <template>
-  <div>
+  <div mt-10>
     <h1 text-6>
       MineSweeper
     </h1>
+
     <div>
-      <div v-for="y in 10" :key="y">
-        <button v-for="x in 10" :key="x" w-10 h-10 border>
-          {{ (y - 1) * 10 + x }}
+      <div v-for="(row, y) in state" :key="y">
+        <button v-for="(item, x) in row" :key="x" w-10 h-10 border hover:bg-gray @click="onClick(x, y)">
+          {{ item.x }},{{ item.y }}
         </button>
       </div>
     </div>
